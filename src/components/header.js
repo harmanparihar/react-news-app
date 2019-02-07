1ac393563cfa04ddc2ea3222b4bc90f549c0867f
harmanparihar <harmanparihar18@gmail.com>
2019-02-06 22:48:09 -0800

    first_commit

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from './search'
+import Menu from './menu'
+
+const Header = () => (
+    <div>
+        Header
+        <Search />
+        <Menu />
+    </div>
+)
+export default Header
---
+++ src/components/menu.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+
+
+const styles = (theme) => ({
+  root: {
+    flex: 1,
+  },
+  paper: {
+    height: 80,
+    border: '2px solid rgb(10,10,100)',
+  },
+  label: {
+    padding: '1rem 0',
+    fontSize: '18px',
+    [theme.breakpoints.down('709')]: {
+      paddingTop: '.5rem',
+      paddingBottom: '.5rem',
+    },
+  },
+})
+class Menu extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <BottomNavigation
+        value={value}
+        onChange={this.handleChange}
+        showLabels
+        className={classes.root}
+      >
+        <BottomNavigationAction classes={{ root: this.props.classes.paper, label: this.props.classes.label, selected: this.props.classes.selected }} value="ca" label="Canada"  />
+        <BottomNavigationAction classes={{ root: this.props.classes.paper, label: this.props.classes.label, selected: this.props.classes.selected }} value="us" label="USA"  />
+        <BottomNavigationAction classes={{ root: this.props.classes.paper, label: this.props.classes.label, selected: this.props.classes.selected }} value="search" label="Search Results"  />
+        <BottomNavigationAction classes={{ root: this.props.classes.paper, label: this.props.classes.label, selected: this.props.classes.selected }} value="in" label="India"  />
+        <BottomNavigationAction classes={{ root: this.props.classes.paper, label: this.props.classes.label, selected: this.props.classes.selected }} value="au" label="Australia"/>
+      </BottomNavigation>
+    );
+  }
+}
+
+Menu.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Menu);
