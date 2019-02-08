d54fbc6c8570d0fb58a1352782b06a9022afed1c
harmanparihar <harmanparihar18@gmail.com>
2019-02-07 20:31:59 -0800

    final submission

--- src/App.js
+++ src/App.js
@@ -33,6 +33,27 @@ class App extends Component {
             console.log(error)
     })
   }
+  formsubmit= async (event) => {
+    event.preventDefault()
+    this.setState({search_phrase: event.target.elements[0].value})
+    console.log(event.target.elements[0].value)
+    this.setState({search_phrase: event.target.elements[0].value})
+    console.log(this.state.search_phrase)
+    axios.get(`https://newsapi.org/v2/everything?q=${event.target.elements[0].value}&apiKey=b48b8a1b85024c5d8d1db1fe09167c36`)
+    .then(response => {
+      if (!response.data.errmsg) {
+        this.setState({articles : response.data.articles})
+          console.log('get operation successful')
+          console.log(response.data.articles)
+          
+      } else {
+          console.log('resgister operation failed')
+      }
+    }).catch(error => {
+        console.log('register error: ')
+        console.log(error)
+    })
+  }
   searchNews=() => {
     axios.get(`https://newsapi.org/v2/everything?q=${this.state.search_phrase}&apiKey=b48b8a1b85024c5d8d1db1fe09167c36`)
     .then(response => {
@@ -48,13 +69,6 @@ class App extends Component {
         console.log('register error: ')
         console.log(error)
     })
-  }
-  formsubmit= async (event) => {
-    event.preventDefault()
-    this.setState({search_phrase: event.target.elements[0].value})
-    console.log(event.target.elements[0].value)
-    console.log(this.state.search_phrase)
-    this.searchNews()
   }
   render() {
    
--- src/components/search.js
+++ src/components/search.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Search = (props) => (
     <div>
         <form onSubmit={props.formsubmit}> 
-            <input name="search-field" type="text" placeholder="Keyword or Search Phrase" /> 
+            <input name="search-field" value={props.search_prase} type="text" placeholder="Keyword or Search Phrase" /> 
             <button>Search</button>
         </form>
     </div>
